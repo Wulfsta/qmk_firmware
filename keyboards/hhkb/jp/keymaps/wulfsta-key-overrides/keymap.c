0d8420a3d5aadba1e8d13ac75c0a68e4ff2db95b
Wulfsta <wulfstawulfsta@gmail.com>
2021-03-28 18:19:49 -0400

    wulfsta-key-overrides: utilize custom keycodes to fix override behavior

--- keyboards/hhkb/jp/keymaps/wulfsta-key-overrides/keymap.c
+++ keyboards/hhkb/jp/keymaps/wulfsta-key-overrides/keymap.c
@@ -29,15 +29,30 @@
  */
 
 
+#define MOD_MASK_NONE 0
+
 #define BASE_LAYER (1 << 0)
 #define NUMROW_LAYER (1 << 1)
 #define CMDKEY_LAYER (1 << 2)
 #define NUMPAD_LAYER (1 << 3)
+#define ALL_LAYERS BASE_LAYER | NUMROW_LAYER | CMDKEY_LAYER | NUMPAD_LAYER
+
+
+enum custom_keycodes {
+    O_ESC = SAFE_RANGE,
+    O_MINS,
+    O_PLUS,
+    O_EQL,
+    O_LCBR,
+    O_RCBR,
+    O_LPRN,
+    O_RPRN,
+};
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_JP(
-        KC_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_DEL, KC_BSPC,
+        O_ESC, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS, KC_EQL, KC_DEL, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, MO(1),
@@ -60,33 +75,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT_JP(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, _______,
-        KC_TAB, _______, _______, KC_MINS, KC_PLUS, KC_EQL, _______, KC_1, KC_2, KC_3, _______, _______, _______,
-        _______, KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, _______,
+        KC_TAB, _______, _______, O_MINS, O_PLUS, O_EQL, _______, KC_1, KC_2, KC_3, _______, _______, _______,
+        _______, O_LCBR, O_RCBR, O_LPRN, O_RPRN, _______, _______, KC_4, KC_5, KC_6, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_0, _______, _______, _______, _______, _______, _______),
 };
 
 
-const key_override_t l0_kc_esc_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_ESC, KC_TILD, BASE_LAYER); // In case Shift + Esc is needed
-const key_override_t l1_kc_mins_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_MINS, KC_SLSH, NUMPAD_LAYER);
-const key_override_t l1_kc_plus_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_PLUS, KC_ASTR, NUMPAD_LAYER);
-const key_override_t l1_kc_eql_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_EQL, KC_CIRC, NUMPAD_LAYER);
-const key_override_t l1_kc_lcbr_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LCBR, KC_LBRC, NUMPAD_LAYER);
-const key_override_t l1_kc_rcbr_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RCBR, KC_RBRC, NUMPAD_LAYER);
-const key_override_t l1_kc_lprn_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_LPRN, KC_LT, NUMPAD_LAYER);
-const key_override_t l1_kc_rprn_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_RPRN, KC_GT, NUMPAD_LAYER);
+const key_override_t o_esc_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_ESC, KC_ESC, ALL_LAYERS, MOD_MASK_SHIFT); 
+const key_override_t l0_o_esc_override = ko_make_with_layers(MOD_MASK_SHIFT, O_ESC, KC_TILD, BASE_LAYER); // In case Shift + Esc is needed, only do this on layer 0.
+
+const key_override_t l3_o_mins_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_MINS, KC_MINS, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_mins_override = ko_make_with_layers(MOD_MASK_SHIFT, O_MINS, KC_SLSH, NUMPAD_LAYER);
+
+const key_override_t l3_o_plus_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_PLUS, KC_PLUS, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_plus_override = ko_make_with_layers(MOD_MASK_SHIFT, O_PLUS, KC_ASTR, NUMPAD_LAYER);
+
+const key_override_t l3_o_eql_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_EQL, KC_EQL, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_eql_override = ko_make_with_layers(MOD_MASK_SHIFT, O_EQL, KC_CIRC, NUMPAD_LAYER);
+
+const key_override_t l3_o_lcbr_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_LCBR, KC_LCBR, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_lcbr_override = ko_make_with_layers(MOD_MASK_SHIFT, O_LCBR, KC_LT, NUMPAD_LAYER);
+
+const key_override_t l3_o_rcbr_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_RCBR, KC_RCBR, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_rcbr_override = ko_make_with_layers(MOD_MASK_SHIFT, O_RCBR, KC_GT, NUMPAD_LAYER);
+
+const key_override_t l3_o_lprn_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_LPRN, KC_LPRN, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_lprn_override = ko_make_with_layers(MOD_MASK_SHIFT, O_LPRN, KC_LBRC, NUMPAD_LAYER);
+
+const key_override_t l3_o_rprn_default = ko_make_with_layers_and_negmods(MOD_MASK_NONE, O_RPRN, KC_RPRN, NUMPAD_LAYER, MOD_MASK_SHIFT);
+const key_override_t l3_o_rprn_override = ko_make_with_layers(MOD_MASK_SHIFT, O_RPRN, KC_RBRC, NUMPAD_LAYER);
 
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &l0_kc_esc_override,
-    &l1_kc_mins_override,
-    &l1_kc_plus_override,
-    &l1_kc_eql_override,
-    &l1_kc_lcbr_override,
-    &l1_kc_rcbr_override,
-    &l1_kc_lprn_override,
-    &l1_kc_rprn_override,
+    &o_esc_default,
+    &l0_o_esc_override,
+    &l3_o_mins_default,
+    &l3_o_mins_override,
+    &l3_o_plus_default,
+    &l3_o_plus_override,
+    &l3_o_eql_default,
+    &l3_o_eql_override,
+    &l3_o_lcbr_default,
+    &l3_o_lcbr_override,
+    &l3_o_rcbr_default,
+    &l3_o_rcbr_override,
+    &l3_o_lprn_default,
+    &l3_o_lprn_override,
+    &l3_o_rprn_default,
+    &l3_o_rprn_override,
     NULL // Null terminate the array of overrides!
 };
 
